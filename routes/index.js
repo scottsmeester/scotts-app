1776e54d826792a932260b2fd910b428ba40ba1b
Scott Smeester <scott@smeester.com>
2016-02-27 09:13:51 -0700

    committing a reusable express/jade install

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+    { title: 'Scotts Apps',
+      desc: 'My personal apps for business usage.' });
 });
 
 module.exports = router;
